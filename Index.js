36be63bec189b5f2e24b8b5f850950ed09e1e79b
Kellen Presta <kpresta04@gmail.com>
2020-02-14 11:03:18 -0600

    guess working

--- Index.js
+++ Index.js
@@ -1,4 +1,5 @@
 const Word = require("./Word.js");
+const inquirer = require("inquirer");
 
 class Hangman {
   constructor(word) {
@@ -6,11 +7,31 @@ class Hangman {
     this.word = new Word(word);
     this.turn();
   }
-  turn() {
+  async getUserGuess() {
+    let guessPrompt = await inquirer.prompt({
+      message: "Guess a letter",
+      name: "letter"
+    });
+
+    return guessPrompt.letter;
+  }
+  async turn() {
     this.word.displayWord();
     if (this.guessedArray.length > 0) {
       console.log(`Guessed so far: ${this.guessedArray}`);
     }
+
+    let guess = await this.getUserGuess();
+    guess = guess.toString();
+    // console.log(typeof guess);
+    this.word.guess(guess);
+
+    // this.getUserGuess().then(() => {
+    //   getData(username);
+    // });
+    // console.log(userGuess);
+    // this.word.guess(userGuess);
+    this.word.displayWord();
   }
 }
 
