54c73e49bd0b0e4263036ea9059e56fd9504acce
Kellen Presta <kpresta04@gmail.com>
2020-02-14 10:21:44 -0600

    creating game logic

--- Index.js
+++ Index.js
@@ -1,4 +1,17 @@
 const Word = require("./Word.js");
 
-const hello = new Word("hello");
-hello.displayWord();
+class Hangman {
+  constructor(word) {
+    this.guessedArray = []; //array of letters that have been guessed
+    this.word = new Word(word);
+    this.turn();
+  }
+  turn() {
+    this.word.displayWord();
+    if (this.guessedArray.length > 0) {
+      console.log(`Guessed so far: ${this.guessedArray}`);
+    }
+  }
+}
+
+const game = new Hangman("hello");
